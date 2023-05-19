139cf6ad902832d8204c8d6decabb083f14a1613
Sean Vizconde <scvizconde@up.edu.ph>
2023-05-19 15:56:16 +0800

    image and pdf support for email

--- send_email.py
+++ send_email.py
@@ -8,26 +8,44 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2.credentials import Credentials
 
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
-def send_message(sender, receiver, subject, content, cc = None):
+def send_message(sender, receiver, subject, content, images : list = None, pdfs : list = None, cc = None):
 
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     try:
         service = build('gmail', 'v1', credentials = creds)
-        message = MIMEText(content, 'html')
-
+        
+        message = MIMEMultipart()
         message['To'] = receiver
         message['From'] = sender
         message['Subject'] = subject
         message['cc'] = cc
 
-        encoded_message = base64.urlsafe_b64encode(bytes(message.as_string(), "utf-8")).decode("utf-8")
+        message_text = MIMEText(content, 'html')
+        message.attach(message_text)
+
+        if images:
+            for image in images:
+                with open(image, 'rb') as content_image:
+                    img = MIMEImage(content_image.read())
+                    img.add_header('Content-Disposition', 'attachment', filename = "test.jpg")
+                    message.attach(img)
+
+        if pdfs: 
+            for pdf in pdfs: 
+                with open(pdf, 'rb') as content_file:
+                    file = MIMEApplication(content_file.read())
+                    file.add_header('Content-Disposition', 'attachment', filename = "test.pdf")
+                    message.attach(file)
+
         create_message = {
-            'raw': encoded_message
+            'raw': base64.urlsafe_b64encode(bytes(message.as_string(), "utf-8")).decode("utf-8")
         }
 
         send_message = service.users().messages().send(userId="me", body=create_message).execute()
@@ -35,4 +53,6 @@ def send_message(sender, receiver, subject, content, cc = None):
         print(F'An error occurred: {error}')
         send_message = None
     return send_message
+
+send_message("scvizconde@up.edu.ph", "jmconcepcion6@up.edu.ph", "ABCDEFG", "a <br> <h1>test</h1>", ["test_image.jpg"], ["INSTRUCTIONS.pdf"])
 
