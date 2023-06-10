e8a21b37002e389c8e91c764b104eb63b6a9acf6
Sean Vizconde <scvizconde@up.edu.ph>
2023-06-10 22:16:12 +0800

    a

--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -1,5 +1,5 @@
-console.log('loaded');
 jQuery(document).ready(function () {
+  
   // click on next button
   jQuery('.form-wizard-next-btn').click(function () {
     var parentFieldset = jQuery(this).parents('.wizard-fieldset');
@@ -10,14 +10,22 @@ jQuery(document).ready(function () {
     var nextWizardStep = true;
     parentFieldset.find('.wizard-required').each(function () {
       var thisValue = jQuery(this).val();
-
-      if (thisValue == '') {
-        jQuery(this).siblings('.wizard-form-error').slideDown();
+      if (thisValue == '' || thisValue == null) {
+        jQuery(this).siblings('.wizard-form-error').slideDown('300');
         nextWizardStep = false;
       } else {
-        jQuery(this).siblings('.wizard-form-error').slideUp();
+        jQuery(this).siblings('.wizard-form-error').slideUp('300');
       }
     });
+    
+    // var temp_checked = jQuery('input.quiz_checkbox:checked');
+    // var quiz_checked = parentFieldset.find(temp_checked).length;
+    // if (quiz_checked <= 0) {
+    //   nextWizardStep = false;
+    //   alert('Please request at least one form/certificate.');
+    // } else {
+    //   nextWizardStep = true;
+    // }
     if (nextWizardStep) {
       next.parents('.wizard-fieldset').removeClass('show', '400');
       currentActiveStep
@@ -51,6 +59,7 @@ jQuery(document).ready(function () {
           }
         });
     }
+    
   });
   //click on previous button
   jQuery('.form-wizard-previous-btn').click(function () {
@@ -99,10 +108,10 @@ jQuery(document).ready(function () {
       .find('.form-wizard-steps .active');
     parentFieldset.find('.wizard-required').each(function () {
       var thisValue = jQuery(this).val();
-      if (thisValue == '') {
-        jQuery(this).siblings('.wizard-form-error').slideDown();
+      if (thisValue == '' || thisValue == null) {
+        jQuery(this).siblings('.wizard-form-error').slideDown('300');
       } else {
-        jQuery(this).siblings('.wizard-form-error').slideUp();
+        jQuery(this).siblings('.wizard-form-error').slideUp('300');
       }
     });
   });
@@ -110,7 +119,7 @@ jQuery(document).ready(function () {
   jQuery('.form-control')
     .on('focus', function () {
       var tmpThis = jQuery(this).val();
-      if (tmpThis == '') {
+      if (tmpThis == '' || tmpThis == null) {
         jQuery(this).parent().addClass('focus-input');
       } else if (tmpThis != '') {
         jQuery(this).parent().addClass('focus-input');
@@ -118,7 +127,7 @@ jQuery(document).ready(function () {
     })
     .on('blur', function () {
       var tmpThis = jQuery(this).val();
-      if (tmpThis == '') {
+      if (tmpThis == '' || tmpThis == null) {
         jQuery(this).parent().removeClass('focus-input');
         jQuery(this).siblings('.wizard-form-error').slideDown('3000');
       } else if (tmpThis != '') {
@@ -127,3 +136,86 @@ jQuery(document).ready(function () {
       }
     });
 });
+
+function onKeyDown(evt) {
+  if (['e', 'E', '+', '-', '.', '='].includes(evt.key)) {
+    evt.preventDefault();
+  }
+}
+function maxLengthCheck(object) {
+  const requiredLength = 9;
+  if (object.value.length > requiredLength) {
+    object.value = object.value.slice(0, requiredLength);
+  }
+
+  if (object.value.length !== requiredLength) {
+    object.classList.add('error'); // Add a CSS class to highlight the input
+    const errorElement = document.querySelector('.wizard-form-error-msg');
+    errorElement.innerText = `Input must be exactly ${requiredLength} characters long.`;
+  } else {
+    object.classList.remove('error'); // Remove the CSS class
+    const errorElement = document.querySelector('.wizard-form-error-msg');
+    errorElement.innerText = '';
+  }
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validateEmailInput() {
+  const emailElement = document.getElementById('email');
+  const emailErrorElement = emailElement.nextElementSibling;
+
+  if (!validateEmail(emailElement.value.trim())) {
+    emailElement.classList.add('error');
+    emailErrorElement.innerText = emailElement.value.trim() === '' ? '' : 'Email should contain at least "@" and "."';
+  } else {
+    emailElement.classList.remove('error');
+    emailErrorElement.innerText = '';
+  }
+}
+
+function validateForm() {
+  const emailElement = document.getElementById('email');
+  const emailErrorElement = emailElement.nextElementSibling;
+
+  if (!validateEmail(emailElement.value.trim())) {
+    emailElement.classList.add('error');
+    emailErrorElement.innerText = emailElement.value.trim() === '' ? '' : 'Email should contain at least "@" and "."';
+    return false;
+  } else {
+    emailElement.classList.remove('error');
+    emailErrorElement.innerText = '';
+  }
+  return true;
+}
+
+
+
+/* function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validateForm() {
+  const emailElement = document.getElementById('email');
+  const emailValue = emailElement.value.trim();
+
+  if (!validateEmail(emailValue)) {
+    emailElement.classList.add('error'); // Add a CSS class to highlight the input
+    const errorElement = document.querySelector('.wizard-form-error-msg');
+    errorElement.innerText = 'Please enter a valid email address.';
+    return false; // Prevent form submission
+  } else {
+    emailElement.classList.remove('error'); // Remove the CSS class
+    const errorElement = document.querySelector('.wizard-form-error-msg');
+    errorElement.innerText = '';
+    return true; // Allow form submission
+  }
+}
+ */
+// Get the total amount from the query parameters
+const urlParams = new URLSearchParams(window.location.search);
+const totalAmount = urlParams.get('total_amount');
