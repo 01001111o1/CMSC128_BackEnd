ef57bd8c99793736d0cf6fa4ccd6d5232e009131
Sean Vizconde <scvizconde@up.edu.ph>
2023-06-17 19:18:28 +0800

    aaaaaaa

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,3 +1,11 @@
+"""
+
+File contains the routes that are accessible from the admin side (login required)
+
+2023 UPB2GO
+
+"""
+
 from flask import render_template, request, redirect, jsonify, make_response, url_for, session, flash, Blueprint, jsonify
 from flask_login import login_required, current_user
 from . import db
@@ -5,41 +13,44 @@ from app import app, executor, scheduler
 from .models import Request
 from .email_template import email_template
 import shutil
-
 import time
-
 from datetime import date
 import ast 
-
 from send_email import send_message
-
 from .Lists import Documents
-
 import pythoncom
-
-from .send_generated_files import background_runner
-
+from .background_runner import background_runner
 from flask_paginate import Pagination
-
 from werkzeug.test import create_environ
 from werkzeug.urls import iri_to_uri
 from werkzeug.wsgi import get_current_url
-
 from sqlalchemy.orm.exc import NoResultFound
-
 from payment_processing import payment_received
-
 from datetime import datetime
 import pytz
-
 import os.path
 
 admin_views = Blueprint('admin_views', __name__)
 
+"""
+Route that displays the admin login page (only page that doesnt require logging in)
+
+"""
 @admin_views.route("/admin/login")
 def admin_login():
     return render_template("admin/admin-login.html", user = current_user)
+"""
 
+Route that displays the admin dashboard
+
+Implements pagnination to prevent the server from accessing all records at once and instead displays only a fixed number (5) of entries at a time
+
+a url is stored in the session variable to maintain the current page and sort criteria on change / update
+
+In the case where an entry is rejected for some reason, a POST request is sent to this route and is processed by sending an email which contains
+the reaosn for rejecting said entry and deleting it from the database as well as removing the folder from the server's storage
+
+"""
 @admin_views.route("/admin/dashboard/<parameter>/", methods = ["GET", "POST"])
 @login_required
 def admin_dashboard(parameter):
@@ -80,6 +91,11 @@ def admin_dashboard(parameter):
 
     return render_template("admin/dashboard.html", pages = pages, documents = Documents, user = current_user, parameter = parameter)
 
+"""
+Route that handles any changes to any toggle-able part of the dashboard
+
+Any click event to one of these toggle-able elements is handled accordingly and an appropriate email is sent to the requester for a specific entry
+"""
 @admin_views.route("/update/<int:queue_number>/<classification>")
 @login_required
 def update(queue_number, classification):
@@ -94,7 +110,7 @@ def update(queue_number, classification):
 
         exec(f'query.{classification} = True')
 
-        if classification == "request_paid":
+        if classification == "request_paid" and query.payment_date is None: 
             query.payment_date = datetime.now(pytz.timezone('Singapore')).replace(microsecond = 0)
 
         db.session.commit()
@@ -104,6 +120,13 @@ def update(queue_number, classification):
         flash("error sending update email", "error")
         return redirect(session["url"])
 
+
+"""
+Route that handles the case where a transaction is finished (has been claimed)
+
+An email containing the receipt of the transaction is sent to the requester.
+
+"""
 @admin_views.route("/delete/<int:queue_number>")
 @login_required
 def delete_entry(queue_number):
@@ -119,8 +142,17 @@ def delete_entry(queue_number):
         flash("Error deleting transaction", "error")
         return redirect(session["url"])
 
-@admin_views.route("/remove/<int:queue_number>")
-@login_required
+"""
+
+remove_entry: function
+
+parameter: queue_number
+
+Removes the entry from the database as well as any folders associated with that entry
+
+
+"""
+
 def remove_entry(queue_number):
     query = Request.query.get_or_404(queue_number)  
     folder_name = " ".join([query.first_name.upper(), query.middle_name.upper(), query.last_name.upper()])
@@ -133,13 +165,10 @@ def remove_entry(queue_number):
     if os.path.isdir(folder_path):
         shutil.rmtree(folder_path)
         
-    try:
-        db.session.delete(query)
-        db.session.commit()
-        flash("Entry successfully deleted", "success")
-        return redirect(session["url"])
-    except:
-        flash("Error deleting transaction", "error")
-        return redirect(session["url"])
+    db.session.delete(query)
+    db.session.commit()
+    flash("Entry successfully deleted", "success")
+    return redirect(session["url"])
+
 
 
